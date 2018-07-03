fd728dbb485e62558a2f86ac6c73bcc886d24f7b
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-03 18:06:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,36 @@
 class Student
+    attr_accessor :name, :grade
+
+    def initialize(name, grade, id=nil)
+      @id = id
+      @name = name
+      @grade = grade
+    end
+
+    def save
+      sql = <<- SQL
+        INSERT INTO Student (name, grade)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+    end
+
+
+end
+
+
+
+
+
+
+
+
+
+
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
-end
+  #  with DB[:conn]
